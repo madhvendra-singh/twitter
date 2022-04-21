40b9d6c1ec204a48bdfe19ff7b6f3ccba4a37022
Madhvendra Singh <madhvendraofficial@gmail.com>
2022-04-21 15:58:41 +0530

    make it better

--- src/config.js
+++ src/config.js
@@ -6,8 +6,8 @@
  * access_token_secret
  */
 module.exports = {
-    consumer_key: '',  
-    consumer_secret: '',
-    access_token: '',  
-    access_token_secret: ''
+    consumer_key: 'qesfqVAAW0gGgehp8F5ECVcUN',  
+    consumer_secret: 'RhZsx4ao0A1AisuYnboW0ocwEZn5hhfff9ZdSpB8OUwYrVDcbc',
+    access_token: '1441576083640360968-RFvsiXOLXGupnznI54hPDlHFo5uUE7',  
+    access_token_secret: 'sL9dOLH3j1B9a7HTKffdITyQfP3eNOPIzACVn5AC5bW6u'
   }
